7baeee82f802c5a19465377c86e30228321c1635
Jamie Harding <jharding@apple.com>
2022-01-22 00:29:38 -0800

    mypy fixes, generate coverage report

--- src/package_name/source.py
+++ src/package_name/source.py
@@ -1,12 +1,19 @@
 #!/bin/env python
 
 def hi(a, b):
+    # delete lines 5-10 to create a flake8 error.
+    #   line  5
+    #   line  6
+    #   line  7
+    #   line  8
+    #   line  9
+    #   line 10
     return a > b
 
 
 # intentional mypy error
 # def staticlytyped(a: int, b: int) -> bool:
-#    return a + b
+#     return a + b
 
 
 def main():
